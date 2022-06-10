22b2667976f8602e957316432a0d808ae084720d
Hermes Júnior <hermesj.junior@gmail.com>
2022-06-09 22:20:50 -0300

    First commit: initial settings

---
+++ index.js
@@ -0,0 +1,20 @@
+import express, { json } from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import chalk from "chalk";
+
+import router from "./routes/index.js";
+
+dotenv.config();
+
+const app = express();
+app.use(cors());
+app.use(json());
+app.use(router);
+
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+    console.log(chalk.green(`mode: ${process.env.MODE || "not defined -> DEV"}`));
+    console.log(chalk.green(`verbose: ${process.env.VERBOSE || "false"}`));
+    console.log(chalk.green(`server is up and running on port ${port}`));
+});
