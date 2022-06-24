b2fb67c45944d9042f430b6661dcd5c00376b077
Briofita09 <felipe.osorio.santos@hotmail.com>
2022-06-24 00:30:35 -0300

    Preparando revisão de código

--- schemas/urlsSchema.js
+++
@@ -1,9 +0,0 @@
-import Joi from "joi";
-
-export const urlSchema = Joi.object({
-    url: Joi.string().required().uri()
-});
-
-export const urlIdSchema = Joi.object({
-    id: Joi.number().integer().required()
-});
